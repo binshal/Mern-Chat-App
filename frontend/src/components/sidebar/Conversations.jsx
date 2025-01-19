3ac442aa2b80d5ae00c17075198cc68e8f34a895
binshal <binshalofc@gmail.com>
2025-01-19 15:07:27 +0530

    3rd part completed - frontend without live server (socket.io)

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,14 +1,23 @@
+import useGetConversations from "../../hooks/useGetConversations";
+import { getRandomEmoji } from "../../utils/emoji";
 import Conversation from "./Conversation";
 
 const Conversations = () => {
+	const { loading, conversations = [] } = useGetConversations(); // Ensure conversations is an array
+	
 	return (
 		<div className='py-2 flex flex-col overflow-auto'>
-			<Conversation />
-			<Conversation />
-			<Conversation />
-			<Conversation />
-			<Conversation />
-			<Conversation />
+			{conversations.map((conversation, idx) => {
+				return (
+					<Conversation 
+						key={conversation._id}
+						conversation={conversation}
+						emoji={getRandomEmoji()}
+						lastIdx={idx === conversations.length - 1}
+					/>
+				);
+			})}
+			{loading ? <span className='loading loading-spinner'></span> : null}
 		</div>
 	);
 };
--- frontend/src/components/sidebar/LogoutButton.jsx
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -1,8 +1,9 @@
 import { BiLogOut } from "react-icons/bi";
 import useLogout from "../../hooks/useLogout";
 
+
 const LogoutButton = () => {
-	const { loading, logout } = useLogout();
+	const {loading,logout} = useLogout()
 
 	return (
 		<div className='mt-auto'>
