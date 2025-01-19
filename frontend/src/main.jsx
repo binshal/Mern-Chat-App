b39ff526e4f8df380d8829f7e661dfb59820ff4f
binshal <binshalofc@gmail.com>
2025-01-19 16:54:01 +0530

    4th part completed - socket

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,13 +4,16 @@ import { BrowserRouter} from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import { AuthContextProvider } from './context/AuthContext.jsx'
+import { SocketProvider } from './context/SocketContext.jsx'
 
 createRoot(document.getElementById('root')).render(
 
   <StrictMode>
     <BrowserRouter>
     <AuthContextProvider>
-        <App />
+      <SocketProvider>
+      <App />
+      </SocketProvider>   
     </AuthContextProvider>
     </BrowserRouter>
   </StrictMode>,
